b4e19dd65dfaa9542149f60af81936aeff24cff4
Rakhmat Nashrul Wathon <rakhmatnashrul@gmail.com>
2020-05-30 18:47:11 +0700

    Start a server and added cors

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const dotenv = require("dotenv")
 const db = require("./db")
+const express = require("express")
+const app = express()
+const cors = require("cors")
 
 // Initiate dotenv
 dotenv.config();
 
 // Connect to the database
-db.connect(process.env.DB_URL)
+db.connect(process.env.DB_URL);
 
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {console.log(`Server is started on PORT: ${PORT}`)})
