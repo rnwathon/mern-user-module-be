305e03a06f103bee0d29b5206f25d87a8a48ad6e
Rakhmat Nashrul Wathon <rakhmatnashrul@gmail.com>
2020-05-31 04:40:27 +0700

    Created user login controller without encrypting the password yet

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const userServices = require("../services/user")
+const jwtSignPayload = require('../helpers/jwtSignPayload')
 
 exports.createUser = (req, res, next) => {
   const { fullname, email, password } = req.body;
@@ -33,6 +34,70 @@ exports.getUsers = (req, res, next) => {
       success: true,
       message: "Get users success!",
       result
+    })
+  })
+  .catch(err => {
+    res
+    .status(err.statusCode || 500)
+    .json({
+      success: false,
+      message: err.message,
+      result: null
+    })
+  })
+}
+
+exports.loginUser = (req, res, next) => {
+  const { email, password } = req.body;
+
+  userServices.getUserByQuery({email})
+  .then(result => {
+    if(!result){
+      res
+      .status(400)
+      .json({
+        success: false,
+        message: "There is no account associated with the email!",
+        result: null
+      })  
+    }
+
+    if(result.password !== password){
+      res
+      .status(400)
+      .json({
+        success: false,
+        message: "Wrong Password!",
+        result: null
+      }) 
+    }
+
+    const user = result;
+    const payload = {
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email
+    }
+
+    jwtSignPayload(payload, process.env.JWT_KEY)
+    .then(token => {
+      res
+      .status(200)
+      .json({
+        success: true,
+        message: "Login success!",
+        result: payload,
+        token
+      })
+    })
+    .catch(err => {
+      res
+      .status(err.statusCode || 500)
+      .json({
+        success: false,
+        message: err.message,
+        result: null
+      })
     })
   })
   .catch(err => {
